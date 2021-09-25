483c474d50371db675d52c243f42bf7aab74adfe
Takahiro55555 <tkhr.corei7@gmail.com>
2021-09-25 20:03:12 +0900

    refactor: コメントアウト部分を削除

--- meter-recognition.py
+++ meter-recognition.py
@@ -24,28 +24,18 @@ def main():
     img_normalized_03 = normalize(img_fliped_03)
     img_normalized_04 = normalize(img_fliped_04)
     # network = init_network()
-
     sv = np.full((28, 1), 122, np.uint8)  # 区切り線（垂直）
-    # print(img_normalized_01)
     img_normalized_all = np.hstack((img_normalized_04, sv, img_normalized_03, sv, img_normalized_02, sv, img_normalized_01))  # 結合
-    cv2.imwrite("%s.extracted.jpg" % fname, img_normalized_all)
-    # cv2.imshow('img_normalized_all', img_normalized_all)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    cv2.imwrite("%s.extracted.jpg" % fname, img_normalized_all)  # 画像出力
     print("---------------------------")
 
 def clip_meter(img_original):
-    # cv2.imshow('img_original', img_original)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
     #                         y0    y1   x1     x2
     img_01 = img_original[840 : 960, 1190 : 1270]
     img_02 = img_original[840 : 960, 1313 : 1435]
     img_03 = img_original[840 : 960, 1435 : 1557]
     img_04 = img_original[840 : 960, 1558 : 1680]
-
-    # img_cliped = img_original[810 : 990, 1190 : 1680]
+    # img_cliped = img_original[810 : 990, 1190 : 1680]  # メータ整数部（4桁）
     return img_01, img_02, img_03, img_04
 
 def calc_threshold(img_c1, p = 1.3, o = 0.5):
@@ -56,53 +46,13 @@ def calc_threshold(img_c1, p = 1.3, o = 0.5):
 
 def normalize(img_meter):
     img_blue_c1, img_green_c1, img_red_c1 = cv2.split(img_meter)
-    # cv2.imshow('img_red_c1', img_red_c1)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    # cv2.imshow('img_green_c1', img_green_c1)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    # cv2.imshow('img_meter', img_meter)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    # kernel = make_sharp_kernel(3)
-    # img_filtered = cv2.filter2D(img_thresh, -1, kernel).astype("uint8")
-    # cv2.imshow('img_filtered', img_filtered)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-    # dst1 = cv2.medianBlur(img_blue_c1, ksize=7)
-    # cv2.imshow('img_thresh', dst1)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
     img_c1 = img_blue_c1
     threshold = calc_threshold(img_c1)
     ret, img_thresh = cv2.threshold(img_c1, threshold, 255, cv2.THRESH_BINARY)  # 2値化
-    # cv2.imshow('img_thresh', img_thresh)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
     dst2 = cv2.medianBlur(img_thresh, ksize=5)  # メジアンフィルタを適用
-
     opening = cv2.morphologyEx(dst2, cv2.MORPH_OPEN, make_ellipse_kernel())  # 縮小した後、元の大きさに拡大
     opening_sq = transform_to_square(opening)
     img_resized = cv2.resize(opening_sq, dsize=(28, 28), interpolation = cv2.THRESH_BINARY)
-    # cv2.imshow('img_resized', img_resized)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-    # 細線化(スケルトン化) THINNING_ZHANGSUEN
-    # skeleton1   =   cv2.ximgproc.thinning(img_resized, thinningType=cv2.ximgproc.THINNING_ZHANGSUEN)
-    # cv2.imshow('skeleton1', skeleton1)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-    # 細線化(スケルトン化) THINNING_GUOHALL 
-    # skeleton2   =   cv2.ximgproc.thinning(img_resized, thinningType=cv2.ximgproc.THINNING_GUOHALL)
-    # cv2.imshow('skeleton2', skeleton2)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
     return img_resized
 
 def init_network():
