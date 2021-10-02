cc6b887a18fb14257d95f03dd3e1d7ff877f86b9
Takahiro55555 <tkhr.corei7@gmail.com>
2021-10-02 15:09:55 +0900

    update: パラメータの総当たり（100通り）

--- clipper.py
+++ clipper.py
@@ -37,9 +37,6 @@ def main():
         psp_matrix = cv2.getPerspectiveTransform(perspective_base, perspective)
         meter_img = cv2.warpPerspective(src_img, psp_matrix, (width, height))
 
-        clipper_p = 1.3
-        clipper_o = 0.5
-        meter_bin_img = clipper_rgb(meter_img, "B", clipper_p, clipper_o)
 
         # 画像切り取り（img[top : bottom, left : right]）
         single_meter_img_04 = meter_img[40:180, 30:130]
@@ -47,38 +44,55 @@ def main():
         single_meter_img_02 = meter_img[40:180, 300:400]
         single_meter_img_01 = meter_img[40:180, 440:540]
 
-        img_num_04 = filter_gray(clipper_rgb(single_meter_img_04, "B", clipper_p, clipper_o))
-        img_num_03 = filter_gray(clipper_rgb(single_meter_img_03, "B", clipper_p, clipper_o))
-        img_num_02 = filter_gray(clipper_rgb(single_meter_img_02, "B", clipper_p, clipper_o))
-        img_num_01 = filter_gray(clipper_rgb(single_meter_img_01, "B", clipper_p, clipper_o))
-        
-        # デバッグ用
-        # 輝度平均値の取得
-        img_blue_04, _, _ = cv2.split(single_meter_img_04)
-        img_blue_03, _, _ = cv2.split(single_meter_img_03)
-        img_blue_02, _, _ = cv2.split(single_meter_img_02)
-        img_blue_01, _, _ = cv2.split(single_meter_img_01)
+        # 画像2値化
+        clipper_p = 0.6
+        clipper_o = 0.0
+        img_v_list = []
+        for index_p in range(10):
+            clipper_p += 0.1
+            clipper_o = 0.5
+            img_h_list = []
+            for index_o in range(10):
+                clipper_o += 0.1
+                meter_bin_img = clipper_rgb(meter_img, "B", clipper_p, clipper_o)
+                img_num_04 = filter_gray(clipper_rgb(single_meter_img_04, "B", clipper_p, clipper_o))
+                img_num_03 = filter_gray(clipper_rgb(single_meter_img_03, "B", clipper_p, clipper_o))
+                img_num_02 = filter_gray(clipper_rgb(single_meter_img_02, "B", clipper_p, clipper_o))
+                img_num_01 = filter_gray(clipper_rgb(single_meter_img_01, "B", clipper_p, clipper_o))
+                
+                # デバッグ用
+                # 輝度平均値の取得
+                img_blue_04, _, _ = cv2.split(single_meter_img_04)
+                img_blue_03, _, _ = cv2.split(single_meter_img_03)
+                img_blue_02, _, _ = cv2.split(single_meter_img_02)
+                img_blue_01, _, _ = cv2.split(single_meter_img_01)
 
-        brightness_mean_04 = np.mean(img_blue_04)
-        brightness_mean_03 = np.mean(img_blue_03)
-        brightness_mean_02 = np.mean(img_blue_02)
-        brightness_mean_01 = np.mean(img_blue_01)
-        brightness_text = "mean : %.1f, %.1f, %.1f, %.1f" % (brightness_mean_04, brightness_mean_03, brightness_mean_02, brightness_mean_01)
+                brightness_mean_04 = np.mean(img_blue_04)
+                brightness_mean_03 = np.mean(img_blue_03)
+                brightness_mean_02 = np.mean(img_blue_02)
+                brightness_mean_01 = np.mean(img_blue_01)
+                brightness_text = "mean : %.1f, %.1f, %.1f, %.1f" % (brightness_mean_04, brightness_mean_03, brightness_mean_02, brightness_mean_01)
 
-        # 切り取り画像の結合
-        #    np.full((h, w, dim), val, np.uint8)
-        sv = np.full((140, 5), 140, np.uint8)  # 区切り線（垂直）
-        clipped_img = cv2.cvtColor(cv2.hconcat((img_num_04, sv, img_num_03, sv, img_num_02, sv, img_num_01)), cv2.COLOR_GRAY2BGR)  # 結合
-        h, w, c = clipped_img.shape[:3]
-        pl = (width - w) - int((width - w)/2)
-        pt = (height - h) - int((height - h)/2)
-        clipped_img_with_padding = cv2.copyMakeBorder(clipped_img, pt, int((height - h)/2), pl, int((width - w)/2), cv2.BORDER_CONSTANT, value=(0, 0, 255))
+                # 切り取り画像の結合
+                #    np.full((h, w, dim), val, np.uint8)
+                sv = np.full((140, 5), 140, np.uint8)  # 区切り線（垂直）
+                clipped_img = cv2.cvtColor(cv2.hconcat((img_num_04, sv, img_num_03, sv, img_num_02, sv, img_num_01)), cv2.COLOR_GRAY2BGR)  # 結合
+                h, w, c = clipped_img.shape[:3]
+                pl = (width - w) - int((width - w)/2)
+                pt = (height - h) - int((height - h)/2)
+                clipped_img_with_padding = cv2.copyMakeBorder(clipped_img, pt, int((height - h)/2), pl, int((width - w)/2), cv2.BORDER_CONSTANT, value=(0, 0, 255))
 
-        texts = ["src: %s" % src_file, "expected: %s" % num, "p: %.3f, o: %.3f" % (clipper_p, clipper_o), brightness_text]
-        comment_img = cv2.cvtColor(make_text_image(width, int(height / 4 * 3), texts), cv2.COLOR_GRAY2BGR)
-        meter_bin_img = cv2.cvtColor(meter_bin_img, cv2.COLOR_GRAY2BGR)
-        dst_img = cv2.vconcat((meter_img, meter_bin_img, clipped_img_with_padding, comment_img))
-        cv2.imwrite(dst_path, dst_img)
+                texts = ["src: %s" % src_file, "expected: %s" % num, "p: %.3f, o: %.3f" % (clipper_p, clipper_o), brightness_text]
+                comment_img = cv2.cvtColor(make_text_image(width, int(height / 4 * 3), texts), cv2.COLOR_GRAY2BGR)
+                meter_bin_img = cv2.cvtColor(meter_bin_img, cv2.COLOR_GRAY2BGR)
+                result_img = cv2.vconcat((meter_img, meter_bin_img, clipped_img_with_padding, comment_img))
+                
+                #    np.full((h, w, dim), val, np.uint8)
+                h, _, _ = result_img.shape[:3]
+                sv = cv2.cvtColor(np.full((h, 5), 140, np.uint8), cv2.COLOR_GRAY2BGR)  # 区切り線（垂直）
+                img_h_list += [result_img, sv]
+            img_v_list += [cv2.hconcat(img_h_list)]
+        cv2.imwrite(dst_path, cv2.vconcat(img_v_list))
 
 def make_text_image(w, h, texts = [], padding_px=10, font_scale=2, font=cv2.FONT_HERSHEY_PLAIN, font_thickness=1):
     # 参考: https://teratail.com/questions/289837
@@ -148,6 +162,10 @@ def filter_gray(img):
 def calc_threshold(img_c1, p=1.3, o=0.5):
     mean_c1 = np.mean(img_c1)
     threshold = int(mean_c1 * p + o)
+    if threshold < 0:
+        threshold = 0
+    if 255 < threshold:
+        threshold = 255
     return threshold
 
 def make_rectangle_kernel(size=5):
