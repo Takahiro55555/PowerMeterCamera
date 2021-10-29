0b4bb84fa8fbb18a56629b9c140257fdf4b4a751
Takahiro55555 <tkhr.corei7@gmail.com>
2021-10-29 12:27:00 +0900

    fix: ハードコードされていた部分の修正、エラー時の対応処理を追加

--- img_downloader.sh
+++ img_downloader.sh
@@ -15,7 +15,7 @@ IMG_DIR="data/img/src"
 COPYED_IMG_LIST="data/copied.txt"
 
 # ダウンロード元のラズパイのホスト名
-PI_HOSTNAME="10.0.0.41"
+PI_HOSTNAME="pi0wh-01"
 
 # ダウンロード元のラズパイから ICMP エコー応答が返ってくるか確認する
 ICMP_REACHABLE=$(ping -c1 ${PI_HOSTNAME} | grep ttl | wc -l || : )
@@ -29,7 +29,7 @@ fi
 # NOTE: ssh の引数にホスト名を直接渡すと5回に4回位の割合でエラーを吐くため、
 #       ホスト名から IPv4アドレスを割り出し、当該アドレスを引数に渡す
 # TODO: 上記のエラーの原因は、今後調査する
-PI_IPV4_ADDRESS=$(host pi0wh-01 | head -n1 | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}')
+PI_IPV4_ADDRESS=$(host ${PI_HOSTNAME} | head -n1 | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}')
 echo "Pi IPv4 Address: ${PI_IPV4_ADDRESS}"
 
 # ファイル名に '.copied' が付加されていない画像ファイルから一番古い日付のファイル名を抽出する
@@ -42,13 +42,13 @@ TOP_IMG_NAME=$(ssh pi@${PI_IPV4_ADDRESS} -p 22 'ls ${HOME}/img/*.png | head -n1'
 #   画像ファイルの数が、適当に設定した値の数を超える前にSDカードの容量が足りなくなるため問題は無いと考える。
 ls ${IMG_DIR}/*.png | \
     grep -A 99999999999999999999999 ${TOP_IMG_NAME} | \
-    grep -oE "[0-9a-zA-Z_\-]+\.png$" > ${COPYED_IMG_LIST}
+    grep -oE "[0-9a-zA-Z_\-]+\.png$" > ${COPYED_IMG_LIST} || :
 
 # 上記で抽出したファイル一覧を保存したテキストファイルをラズパイに転送する
 scp ${COPYED_IMG_LIST} pi@${PI_IPV4_ADDRESS}:~/copied.txt
 
 # 上記で転送したテキストファイルに記載されているファイル名を変更する
-ssh pi@${PI_IPV4_ADDRESS} -p 22 'cat copied.txt | xargs -I{} mv img/{} img/{}.copied'
+ssh pi@${PI_IPV4_ADDRESS} -p 22 'cat ~/copied.txt | xargs -I{} mv ~/img/{} ~/img/{}.copied'
 
 # まだダウンロードしていない画像ファイルをダウンロードする
 time scp pi@${PI_IPV4_ADDRESS}:~/img/*.png data/img/src
